c60b8aa4dd46ab884050b795d5cc2eae9dd0be57
DarkCode2317 <142730570+DarkCode2317@users.noreply.github.com>
2026-07-28 19:29:57 +0700

    Update build-android.sh

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -21,8 +21,8 @@ TOOLCHAIN="$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64"
 case "$ABI" in
 
 armeabi-v7a)
-    TARGET=armv7a-linux
-    HOST=arm-linux-androideabi
+    TARGET=armv7a-linux-android
+    HOST=arm-linux
     EXTRA="--extra-cflags='-march=armv7-a -mfpu=neon -mfloat-abi=softfp'"
     export CFLAGS="-march=armv7-a -mfpu=neon -mfloat-abi=softfp"
     ;;
